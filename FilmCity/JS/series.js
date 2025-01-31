4a50639bbb1e8d64fb16d28f03205cd4967bb55f
DavidLopezMirasierra16 <dlm1652003@gmail.com>
2025-01-31 17:01:27 +0100

    series finalizada

---
+++ FilmCity/JS/series.js
@@ -0,0 +1,39 @@
+const caja_mostrar_series = document.querySelector(".series_catalogo");
+
+function leerSeries(){
+
+    fetch("JS/peliculas-series.json", {
+    }).then((response)=>{
+        return response.json();
+    }).then((datos)=>{
+        const series = datos.series;
+        series.forEach(serie => {
+            const caja_serie = document.createElement("div");
+            let mostrar = `
+                            <img src="${serie.Poster}"></img>
+                        `;
+            caja_serie.innerHTML=mostrar;
+            caja_serie.classList.add("bottom");
+            caja_mostrar_series.appendChild(caja_serie);
+
+            caja_serie.addEventListener("mouseenter", () => {
+                const datos = [];
+                //Guardamos los datos de la serie en un array
+                datos.push({
+                    "Temporadas": serie.totalSeasons, 
+                    "Rated": serie.Rated, 
+                    "Genero": serie.Genre
+                });
+
+                console.log(datos);
+            });
+
+        });
+
+    }).catch((error)=>{
+        console.log(error);
+    });
+
+}
+
+leerSeries();
